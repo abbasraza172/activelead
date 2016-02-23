66f8cdcc6b260a37c9548f27fc9156fe5f4c6153
Hasan Zia <hasan.zia@engintechnologies.com>
2016-02-23 20:52:15 +0500

    Add Main/Admin Layouts + Update routes + Add landing page

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require main/jquery.1.10.2
+//= require main/jquery.loupe.min
+//= require main/tweetie
+//= require main/jquery.placeholder
+//= require main/waypoints.min
+//= require main/waypoints-sticky.min
+//= require main/jquery.countTo
+//= require main/jquery.parallax-1.1.3
+//= require main/jquery.nicescroll.min
+//= require main/custom
+//= require main/animations
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+
+  layout "main"
+
+  def index
+    # render :layout => false
+  end
+
+end
