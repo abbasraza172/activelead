97c8bf4441b8e761943152f83c4471a406a38d7c
Hasan Zia <hasan.zia@engintechnologies.com>
2016-02-29 19:17:34 +0500

    Front end polishing + Fix Migration issue

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -5,11 +5,19 @@ class LeadsController < ApplicationController
 
   def index
     @leads = @list.leads.all
-    @recent_most = @list.leads.recent_most
+    @recent_most = @list.leads.recent_most.last
   end
 
   def show
 
+  end
+
+  def destroy
+
+  end
+
+  def create
+    root_path
   end
 
 private
--- db/migrate/20160226072139_change_plan_type_to_plan_id_in_lists.rb
+++
@@ -1,6 +0,0 @@
-class ChangePlanTypeToPlanIdInLists < ActiveRecord::Migration
-  def change
-    remove_column :lists, :plan_type, :string
-    add_column :lists, :plan_id, :integer
-  end
-end
